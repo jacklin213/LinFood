3434a21ac262936ebcbbf6c557fbbcf45501db32
jack lin <jacklin213@gmail.com>
2013-04-22 09:22:08 +1200

    First commit (With bugs)

---
+++ src/me/jacklin213/linfood/LFMessages.java
@@ -0,0 +1,50 @@
+package me.jacklin213.linfood;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class LFMessages {
+	
+	public static LinFood plugin;
+	public String pluginName = "LinFood";
+	
+	public String author = "jacklin213";
+	public String chatPluginName = ChatColor.WHITE + "[" + ChatColor.GREEN + pluginName + ChatColor.WHITE + "] ";
+	public String description = "A /give plugin for food only";
+	public String invalidCommand = ChatColor.RED + "Not a valid command , Do /linfood help";
+	public String invalidNum = ChatColor.RED + "Not a Valid number";
+	public String playerOnly = ChatColor.RED + "This is a player only Command!";
+	public String permMessage = ChatColor.RED + "You do not have the permissions to use this command!";
+	public String version = "v1.0";
+	
+	public LFMessages(LinFood instance) {
+		plugin = instance;
+	}
+
+	public void basicInfo(CommandSender sender){
+		sender.sendMessage(chatPluginName + "made by" + ChatColor.GOLD + " jacklin213"); 
+	}
+	
+	public void info(CommandSender sender){
+		sender.sendMessage(ChatColor.GOLD + " ============ " + chatPluginName + ChatColor.GOLD + " ============ ");
+		sender.sendMessage(ChatColor.GOLD + "Plugin name: " + ChatColor.AQUA + pluginName);
+		sender.sendMessage(ChatColor.GOLD + "Version: " + ChatColor.AQUA + version);
+		sender.sendMessage(ChatColor.GOLD + "Author: " + ChatColor.AQUA  + "by " + author);
+		sender.sendMessage(ChatColor.GOLD + "Description: " + ChatColor.AQUA + description);
+	}
+	
+	public void help(CommandSender sender, int page){
+		try {
+            if (page == 1) {
+                sender.sendMessage(ChatColor.GOLD + "============" + ChatColor.WHITE + "[" + ChatColor.GREEN + pluginName + ChatColor.WHITE + ":" + ChatColor.YELLOW + "Help" + ChatColor.WHITE + "]" + ChatColor.GOLD + " ============ ");
+                sender.sendMessage(ChatColor.GOLD + "Page" + ChatColor.WHITE + "(" + ChatColor.AQUA + "1" + ChatColor.WHITE + "/" + ChatColor.AQUA + "1" + ChatColor.WHITE + ")" );
+                sender.sendMessage(ChatColor.GOLD + "/linfood " + ChatColor.WHITE + ":" + ChatColor.AQUA + "Shows plugin name and author");
+                sender.sendMessage(ChatColor.GOLD + "/linfood info" + ChatColor.WHITE + ":" + ChatColor.AQUA + "Shows the full plugin info");
+                sender.sendMessage(ChatColor.GOLD + "/linfood help" + ChatColor.WHITE + ":" + ChatColor.AQUA + "Displays this page");
+                sender.sendMessage(ChatColor.GOLD + "/linfood give <item> <amount>" + ChatColor.WHITE + ":" + ChatColor.AQUA + "Give command for food");
+            }
+        } catch (NumberFormatException nfe) {
+            sender.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
+        }
+	}
+}
---
+++ src/me/jacklin213/linfood/LinFood.java
@@ -0,0 +1,127 @@
+package me.jacklin213.linfood;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LinFood extends JavaPlugin{
+	
+	public static LinFood plugin;
+	public Logger log = Logger.getLogger("Minecraft");
+	PluginDescriptionFile pdfFile;
+	
+	public LFMessages lfm = new LFMessages(this);
+	
+	@Override
+	public void onEnable(){
+		log.info(String.format(
+				"[%s] Version %s by jacklin213 has been Enabled!",
+				getDescription().getName(), getDescription().getVersion(),
+				getDescription().getAuthors()));
+	}
+	@Override
+	public void onDisable(){
+		log.info(String.format("[%s] Disabled Version %s", getDescription()
+				.getName(), getDescription().getVersion()));
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[]){
+		if (commandLabel.equalsIgnoreCase("linfood") || (commandLabel.equalsIgnoreCase("lf"))){
+			if (args.length == 1){
+				if (args[0].equalsIgnoreCase("info")){
+					lfm.info(sender);
+					return true;
+				}
+				if (args[0].equalsIgnoreCase("help")){
+					lfm.help(sender, 1);
+					return true;
+				} else {
+					sender.sendMessage(lfm.invalidCommand);
+					return true;
+				}
+			}
+			if (args.length == 2){
+				if (args[0].equalsIgnoreCase("help")){
+					int page = Integer.parseInt(args[1]);
+					lfm.help(sender, page);
+				} else {
+					sender.sendMessage(lfm.invalidCommand);
+					return true;
+				}
+			}
+			if (args.length == 3){
+				if (sender instanceof Player){
+					Player player = (Player) sender;
+					if (args[0].equalsIgnoreCase("give")){
+						String food = args[1];
+						try {
+							int amount = Integer.parseInt(args[2]);
+							addFoodToInv(sender, player, food, amount);
+							return true;
+						} catch (NumberFormatException nfe){
+							player.sendMessage(lfm.invalidNum);
+						}
+					} else {
+						sender.sendMessage(lfm.invalidCommand);
+						return true;
+					}
+				} else {
+					sender.sendMessage(lfm.playerOnly);
+					return true;
+				}
+			}
+			if (args.length > 4){
+				sender.sendMessage(lfm.invalidCommand);
+				return true;
+			}
+			lfm.basicInfo(sender);
+			return true;
+		}
+		
+		
+		return false;
+	}
+	
+	public void addFoodToInv(CommandSender sender, Player p, String food, int qty){
+		try {
+			PlayerInventory inventory = p.getInventory();
+			Material item = Material.getMaterial(food);
+			if (item.isEdible()){
+				inventory.addItem(new ItemStack(item, qty));
+			} else {
+				sender.sendMessage("Invalid Food type");
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+			sender.sendMessage(ChatColor.RED + "Error, Please Check you have typed a valid Food or Amount");
+		}
+	}	
+	
+	/*02:01	Didz	My guess is that youre Material.getMaterial(String) call is returning null
+02:01	Didz	since "carrot" is not a valid name in the Material enum
+02:01	Didz	use matchMaterial instead, and make sure to check if it's null when it returns
+02:02	Didz	s/youre/your/
+02:03		_Rogue_ detects bad regexp
+02:03	jacklin213	kk
+02:03	jacklin213	wat happens if they type a number
+02:03	jacklin213	like the id for carrot
+02:03	Didz	it will return null
+02:03	_Rogue_	if it can't match it re-
+02:03	_Rogue_	
+02:03		*** ModMasta quit (Quit: Web client closed)
+02:04	Didz	if you want to match a number ID, you'll need to use getMaterial(int)
+02:04	Didz	and for that you'll need to Integer.parse the number from the command
+02:04	Didz	so it's an integer not a string
+02:04	Didz	cause it does an entirely different lookup if you pass it a string instead of an integer
+02:04	Didz	yay method overloading
+	 * 
+	 * */
+}
